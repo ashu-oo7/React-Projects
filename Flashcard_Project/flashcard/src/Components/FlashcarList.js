c1335c688839479e1c469c82d813edc4350ac7bf
ashu-oo7 <ashu0705000@gmail.com>
2024-08-16 02:11:13 +0530

    creating Flashcard

---
+++ Flashcard_Project/flashcard/src/Components/FlashcarList.js
@@ -0,0 +1,19 @@
+const sample_cards = [
+    {
+        id:1,
+        question: "What is 3+3?",
+        answer : '4',
+        options :['2','3','5','4']
+    },
+    {
+        id : 2,
+        question: "What is four + four?",
+        answer : "8",
+        options : ['2','3','8','4']
+    },{
+        id : 3,
+        question : "What is six*six",
+        answer : "12",
+        options : ['2','36','5','4']
+    }
+]
